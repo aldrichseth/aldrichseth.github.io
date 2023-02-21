4e193d66aab31bcf9bdc98c0a7c9de4bb68ffffb
slomohh <124629005+slomohh@users.noreply.github.com>
2023-02-21 03:40:28 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,5 @@
 // start button
-const startButton = document.getElementById("startButton");
-startButton.addEventListener("click", () => {
-    const welcomeScreen = document.querySelector(".welcome-screen");
-    const canvas = document.querySelector("canvas");
-    welcomeScreen.style.display = "none";
-    canvas.style.display = "block;
-});
+
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 const grid = 15;
